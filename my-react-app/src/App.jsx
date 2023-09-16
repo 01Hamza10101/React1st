e63471b99811f43528ecda74b29d47345e4199ec
01Hamza10101 <140110689+01Hamza10101@users.noreply.github.com>
2023-09-16 08:26:29 +0000

    React Created

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useCallback, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <img src="./public/vite.svg" alt="" />
+    <div className="name">
+      <h1>Hello,How are you?</h1>
+      <img className="HomeImg" src="./src/assets/Home.jpeg" alt="" />
+    </div>
+    </>
+  )
+}
+
+
+export default App
